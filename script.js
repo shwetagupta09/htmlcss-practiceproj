34098dc80d6bc627bc67d97005018ad38ea20ab3
shwetagupta09 <sg2791999@gmail.com>
2021-08-29 11:29:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlide();
+
+function showSlide(){
+      var i;
+      var slides = document.getElementsByClassName("slide");
+      var dots = document.getElementsByClassName("dot");
+      for(i=0; i< slides.length; i++){
+            slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      for (i=0; i< dots.length; i++){
+            dots[i].className = dots[i].className.replace(" active","");  
+               
+      }
+      slides[slideIndex-1].style.display ="block";
+      dots[slideIndex-1].className += "active";
+      setTimeout(showSlide,5000);
+}
+/* login */
+function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+    
+    function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+    }
+
+   
